c053dbc01983f6dd78a78b8154a00afb64088b33
Markus Staab <maggus.staab@googlemail.com>
2024-12-17 11:35:44 +0100

    Support `#` comments in regex with `x` modifier

---
+++ tests/PHPStan/Analyser/nsrt/bug-12242.php
@@ -0,0 +1,32 @@
+<?php // lint >= 7.4
+
+namespace Bug12242;
+
+use function PHPStan\Testing\assertType;
+
+function foo(string $str): void
+{
+	$regexp = '/
+		# (
+		([\d,]*)
+		# )
+	/x';
+	if (preg_match($regexp, $str, $match)) {
+		assertType('array{string, string}', $match);
+	}
+}
+
+function bar(string $str): void
+{
+	$regexp = '/^
+            (\w+)        # column type [1]
+            [\(]         # (
+                ?([\d,]*)  # size or size, precision [2]
+            [\)]         # )
+            ?\s*         # whitespace
+            (\w*)        # extra description (UNSIGNED, CHARACTER SET, ...) [3]
+        $/x';
+	if (preg_match($regexp, $str, $matches)) {
+		assertType('array{string, non-empty-string, string, string}', $matches);
+	}
+}
